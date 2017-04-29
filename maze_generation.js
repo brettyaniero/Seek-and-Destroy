79af1328256cb935499761d7ab0eb7c65a692ff3
brettyaniero <brett.yani@gmail.com>
2017-04-29 03:13:10 -0500

    Finished maze generation algorithm

--- maze_generation.js
+++ maze_generation.js
@@ -11,7 +11,7 @@ function generateMaze(width, height, startUnit) {
     // Metrics to be used in algorithm calculations
     var unitWidth = stage.canvas.width / width;
     var unitHeight = stage.canvas.height / height;
-    var uncheckedUnits = width * height - 1
+    var uncheckedUnits = width * height - 1;
 
     // Container to store the path shape objects that are created
     var pathContainer = new createjs.Container();
@@ -25,13 +25,23 @@ function generateMaze(width, height, startUnit) {
 
     // Begin maze generation algorithm
     var currentUnit = startUnit;
+
+    // Draw start square
+    var startRow = (currentUnit > width) ? Math.ceil(currentUnit / width) - 1: 0;
+    var startColumn = (currentUnit % width != 0) ? currentUnit % width  - 1: width - 1;
+    var startSquare = new createjs.Shape();
+    startSquare.graphics.beginStroke("black").beginFill("black").drawRect(0, 0, unitWidth, unitHeight);
+    startSquare.x = startColumn * unitWidth;
+    startSquare.y = startRow * unitHeight;
+    pathContainer.addChild(startSquare);
+
     gridStack.push(startUnit);
 
     while (uncheckedUnits > 0) {
-        alert(currentUnit);
         var openPaths = [];
-        var row = (currentUnit > width) ? Math.floor(currentUnit / height) : 1;
-        var column = (currentUnit % width != 0) ? currentUnit % width : width;
+        var row = (currentUnit > width) ? Math.ceil(currentUnit / width)  - 1: 0;
+        var column = (currentUnit % width != 0) ? currentUnit % width - 1: width - 1;
+        // alert("Row: " + row + "\nColumn: " + column);
 
         // Check unit to left (if there is one)
         if (currentUnit > 1 && ((currentUnit - 1) % 7) != 0) {
@@ -66,6 +76,50 @@ function generateMaze(width, height, startUnit) {
 
             // Change current unit to the new unit
             currentUnit = openPaths[index];
+            var newRow = (currentUnit > width) ? Math.ceil(currentUnit / width) - 1 : 0;
+            var newColumn = (currentUnit % width != 0) ? currentUnit % width - 1 : width - 1;
+
+            var path = new createjs.Shape();
+
+            // Check if position has moved horizontally
+            if (newColumn != column) {
+                // Check if position has moved to the left
+                if (column - newColumn === 1) {
+                    path.graphics.beginStroke("black").beginFill("black").drawRect(0, 0, unitWidth, 2 / 3 * unitHeight);
+                    path.regY = (2 / 3 * unitHeight) / 2;
+                    path.y = newRow * unitHeight + unitHeight / 2;
+                    path.x = newColumn * unitWidth + (unitWidth - (2 / 3 * unitWidth)) / 2;
+                    pathContainer.addChild(path);
+                }
+                // Check if position has moved to the right
+                else if (column - newColumn === -1) {
+                    path.graphics.beginStroke("black").beginFill("black").drawRect(0, 0, unitWidth, 2 / 3 * unitHeight);
+                    path.regY = (2 / 3 * unitHeight) / 2;
+                    path.y = newRow * unitHeight + unitHeight / 2;
+                    path.x = column * unitWidth + ((unitWidth - (2 / 3 * unitWidth)) / 2) + (2 / 3 * unitWidth);
+                    pathContainer.addChild(path);
+                }
+            }
+            // Check if position has moved vertically
+            else if (newRow != row) {
+                 // Check if position has moved up
+                if (newRow - row === -1) {
+                    path.graphics.beginStroke("black").beginFill("black").drawRect(0, 0, unitWidth * 2 / 3, unitHeight);
+                    path.regX = (unitWidth * 2 / 3) / 2;
+                    path.x = newColumn * unitWidth + unitWidth / 2;
+                    path.y = newRow * unitHeight + (2/3 * unitHeight) / 4;
+                    pathContainer.addChild(path);
+                }
+                // Check if position has moved down
+                else if (newRow - row === 1) {
+                    path.graphics.beginStroke("black").beginFill("black").drawRect(0, 0, unitWidth * 2 / 3, unitHeight);
+                    
+                    path.regX = (unitWidth * 2 / 3) / 2;
+                    path.x = newColumn * unitWidth + unitWidth / 2;
+                    path.y = row * unitHeight + (2 / 3 * unitHeight) + (2 / 3 * unitHeight) / 4;
+                    pathContainer.addChild(path);
+                }
+            }
 
             uncheckedUnits -= 1;
         }
@@ -76,4 +130,6 @@ function generateMaze(width, height, startUnit) {
             currentUnit = gridStack.pop();
         }
     }
+
+    stage.addChild(pathContainer);
 }
